0ae9ea053f23913bf24995ecbc77e1388de503e2
choimingyu <alsrb1315093@gmail.com>
2025-07-30 07:49:11 +0900

    derivedStateOf 강의자료 만드는중

--- app/src/main/java/com/example/rememberupdatedstate/MainActivity.kt
+++ app/src/main/java/com/example/rememberupdatedstate/MainActivity.kt
@@ -1,47 +1,116 @@
 package com.example.rememberupdatedstate
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.rememberupdatedstate.ui.theme.RememberUpdatedStateTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            RememberUpdatedStateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            var userName by remember { mutableStateOf("최대리") }
+
+            Column (
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+//                CountingCard()
+
+//                AlarmTest(userName)
+//                Button(
+//                    onClick = {
+//                        userName = "김대리"
+//                    }
+//                ) {
+//                    Text("이름바꾸기")
+//                }
+
+                InefficientFilterExample()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun CountingCard(){
+    var count by remember { mutableStateOf(0) }
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "$count"
     )
+    Button(
+        onClick = {
+            count++
+        }
+    ) {
+        Text(
+            text = "+"
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RememberUpdatedStateTheme {
-        Greeting("Android")
+fun AlarmTest(name: String){
+    val context = LocalContext.current
+    val newName by rememberUpdatedState(name)
+
+    LaunchedEffect(Unit) {
+        delay(5000)
+        Toast.makeText(context, "${newName}님 일어나세요", Toast.LENGTH_SHORT).show()
+    }
+}
+
+@Composable
+fun InefficientFilterExample() {
+    val names = remember {
+        List(10000) { "Item #$it" }
+    }
+
+    var query by remember { mutableStateOf("") }
+
+//    val filtered = names.filter {
+//        println("🔁 filtering: $it") // filter가 얼마나 자주 실행되는지 로그 찍음
+//        it.contains(query, ignoreCase = true)
+//    }
+
+    val filtered by remember {
+        derivedStateOf {
+            names.filter {
+                println("🔁 filtering: $it") // filter가 얼마나 자주 실행되는지 로그 찍음
+                it.contains(query, ignoreCase = true)
+            }
+        }
+    }
+
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        TextField(value = query, onValueChange = { query = it })
+        Text("결과 수: ${filtered.size}")
     }
 }
